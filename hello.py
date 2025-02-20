2cdf5b73a5e1ef4fca45f59053978ca1f73c7129
cr1tikal-frimc <marcuswatchdog2@gmail.com>
2025-02-20 13:21:56 +0000

    minor additions

---
+++ hello.py
@@ -0,0 +1,17 @@
+nam = False
+
+c = 3e+8
+
+
+comp2 = 1 + 7j
+comp1 = 4 + 5j
+
+val = 2 * c
+
+calc = comp1 + comp2
+
+data = {"name" : ["Stesyl", "Deborah", "Esi"], "age": [23, 24, 25]}
+
+print(data["name"][1])
+
+#print(calc)
